f3dee47924d8b909f94a7ef0dccb0c6dc9fc77fe
shadowshd <44870949+shadowshd@users.noreply.github.com>
2018-12-26 10:11:06 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1117,11 +1117,11 @@ $("#urltable").change(function () {
     return;
   } else if (trimurl.indexOf("http://www.vpforums.org/") === 0 && trimurl !== "") {
     trimurl = trimurl.replace("http://", "https://");
-    $("#urltable").val(trimurl).change();
+    $("#urltable").val(trimurl);
     toastr.warning("URL VPForums HTTP modifiée en HTTPS.", "Information lien table :");
   } else if (trimurl.indexOf("http://vpinball.com/") === 0 && trimurl !== "") {
     trimurl = trimurl.replace("http://", "https://");
-    $("#urltable").val(trimurl).change();
+    $("#urltable").val(trimurl);
     toastr.warning("URL VPinball HTTP modifiée en HTTPS.", "Information lien table :");
   } else if (trimurl.indexOf("https://mega.nz/") === 0) {
     if (trimurl === "https://mega.nz/" || trimurl === "https://mega.nz") {
